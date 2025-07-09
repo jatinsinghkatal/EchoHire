ceb68d8228c1db3abf95c3cc68ef02f585910106
jatinsinghkatal <jatinsinghkatal@gmail.com>
2025-07-09 17:11:50 +0530

    finished homepage and interview cards ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -28,15 +28,16 @@ const Page = () => {
         <h2>Your Interviews</h2>
         <div className="interviews-section">
           {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview}/>
+            <InterviewCard {...interview} key = {interview.id}/>
           ))}
+          {/* <p> You haven't taken any interviews yet</p> */}
         </div>
       </section>
       <section className = "flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className ="interviews-section">
           {dummyInterviews.map((interview) => (
-            <InterviewCard {...interview}/>
+            <InterviewCard {...interview} key = {interview.id}/>
           ))}
         </div>
       </section>
