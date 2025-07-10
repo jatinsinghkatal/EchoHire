e7604f4228b5cfbc78d773ee85afc735996205a2
jatinsinghkatal <jatinsinghkatal@gmail.com>
2025-07-10 17:06:49 +0530

    server-side auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -8,6 +8,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signUp, signIn } from "@/lib/actions/auth.action";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
 
 import {
   Form,
@@ -45,12 +50,35 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
+        const {name, email, password} = values;
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        const result = await signUp({
+          uid: userCredentials.user.uid, 
+          name: name!,
+          email,
+          password,
+        })
+
+        if(!result?.success){
+            toast.error(result?.message);
+        }
+
         toast.success('Account created successsfully. Please sign in.');
         router.push("/sign-in");
       } else {
+         const {email, password} = values;
+         const userCredential = await signInWithEmailAndPassword(auth, email, password);
+         const idToken = await userCredential.user.getIdToken();
+        if(!idToken){
+          toast.error('Failed to sign in. Please try again later.');
+          return;
+        }
+        await signIn({
+          email, idToken 
+        })
         toast.success('Signed in successfully.');
         router.push("/");
       }
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,8 +1,23 @@
 "use server";
+import { db, auth } from "@/firebase/admin";
+import {cookies} from "next/headers";
+
+const ONE_WEEK = 60 * 60 * 24 * 7;
 
 export async function signUp(params: SignUpParams) {
   const { uid, name, email } = params;
   try {
+    const userRecord = await db.collection('users').doc(uid).get();
+    if(userRecord.exists)
+    {
+        return {
+            success: false, 
+            message: 'User already exists. Please sign in instead.'
+        }
+    }
+    await db.collection('users').doc(uid).set({
+        name, email
+    })
   } catch (e: any) {
     console.log("Error creating a user", e);
     if (e.code === "auth/email-already-exists") {
@@ -11,5 +26,44 @@ export async function signUp(params: SignUpParams) {
         message: "This email is already in use.",
       };
     }
+    return {
+        success : false,
+        message: 'Failed to create an account'
+    }
   }
+}
+
+export async function signIn(params : SignInParams){
+    const {email, idToken} = params;
+    try {
+        const userRecord = await auth.getUserByEmail(email);
+        if(!userRecord){
+            return {
+                success: false,
+                message: 'User not found. Please create an account instead.'
+            }
+        }
+        await setSessionCookie(idToken);
+
+    } catch (e){
+        console.log(e);
+        return {
+            success : false,
+            message : 'Failed to sign in. Please try again later.'
+        }
+    }
+}
+
+export async function setSessionCookie(IdToken: string){
+    const cookieStore = await cookies();
+    const sessionCookie = await auth.createSessionCookie(IdToken, {
+        expiresIn : ONE_WEEK * 1000,
+    })
+    cookieStore.set('session', sessionCookie, {
+        maxAge: ONE_WEEK,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        path: '/',
+        sameSite: 'lax',
+    })
 }
